45043b2bb0f3552f2f08454c07354f3083dbd7b1
Joe <joe@jjssoftware.co.uk>
2021-07-25 08:59:01 +0100

    new client test app

---
+++ cmd/go_auth_api_client/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    goauthapi "github.com/bebo-dot-dev/go-auth-api-v2/client"
+    "os"
+)
+
+func main() {
+
+    configuration := goauthapi.NewConfiguration()
+    apiClient := goauthapi.NewAPIClient(configuration)
+    resp, r, err := apiClient.AuthAPIApi.Ping(context.Background()).Execute()
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Error when calling `AuthAPIApi.Ping``: %v\n", err)
+        fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+    }
+    // response from `Ping`: PingResponse
+    fmt.Fprintf(os.Stdout, "Response from `AuthAPIApi.Ping`: %v\n", resp)
+}
