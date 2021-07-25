39ab78122eba73c66c896b21a3dcc2c57f89ed43
Joe <joe@bebo.dev>
2021-07-25 08:21:29 +0000

    automated codegen commit

--- cmd/go_auth_api_client/main.go
+++ cmd/go_auth_api_client/main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    client "github.com/bebo-dot-dev/go-auth-api-v2/client"
-    "os"
+	"context"
+	"fmt"
+	client "github.com/bebo-dot-dev/go-auth-api-v2/client"
+	"os"
 )
 
 func main() {
 
-    configuration := client.NewConfiguration()
-    apiClient := client.NewAPIClient(configuration)
-    resp, r, err := apiClient.AuthAPIApi.Ping(context.Background()).Execute()
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Error when calling `AuthAPIApi.Ping``: %v\n", err)
-        fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
-    }
-    // response from `Ping`: PingResponse
-    fmt.Fprintf(os.Stdout, "Response from `AuthAPIApi.Ping`: %v\n", resp)
+	configuration := client.NewConfiguration()
+	apiClient := client.NewAPIClient(configuration)
+	resp, r, err := apiClient.AuthAPIApi.Ping(context.Background()).Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `AuthAPIApi.Ping``: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	}
+	// response from `Ping`: PingResponse
+	fmt.Fprintf(os.Stdout, "Response from `AuthAPIApi.Ping`: %v\n", resp)
 }
--- server/README.md
+++ server/README.md
@@ -13,7 +13,7 @@ To see how to make this your own, look here:
 [README](https://openapi-generator.tech)
 
 - API version: 0.1.0
-- Build date: 2021-07-25T07:52:57.629Z[Etc/UTC]
+- Build date: 2021-07-25T08:21:21.405Z[Etc/UTC]
 
 
 ### Running the server
